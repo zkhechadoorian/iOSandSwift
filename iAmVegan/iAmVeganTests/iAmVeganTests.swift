f0cad78e78638d9492bb0742fb0186a35365eae8
zkhechadoorian <zepyoorkhechadoorian@gmail.com>
2025-01-31 18:01:51 -0500

    Created empty 'I am vegan' Xcode project.

---
+++ iAmVegan/iAmVeganTests/iAmVeganTests.swift
@@ -0,0 +1,17 @@
+//
+//  iAmVeganTests.swift
+//  iAmVeganTests
+//
+//  Created by Zepyoor Khechadoorian on 1/31/25.
+//
+
+import Testing
+@testable import iAmVegan
+
+struct iAmVeganTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
